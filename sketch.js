2190bc627ae957b03c231116d6ce28e9b0650221
Andrew Cove <andrew.cove@gmail.com>
2017-05-30 14:54:13 -0400

    Falling bars sketch.

--- sketch.js
+++ sketch.js
@@ -2,15 +2,27 @@
 
 */
 
+var bars=[]
+
 function setup() {
   createCanvas(1280, 720)
   clear()
-  background(51)
+  background(31)
+}
+
+function mouseClicked(){
+  //make a new bar
+  bars.push({x: mouseX, y: mouseY, fill: color(random(20,255),random(20,255),random(20,255))})
 }
 
 function draw() {
-  //clear()
-  //background(51)
-  //noStroke()
-  //ellipseMode(RADIUS)
+  clear()
+  background(31)
+  noStroke()
+
+  bars.forEach(function(b){
+    fill(b.fill)
+    rect(b.x, b.y, 80, 5)
+    b.y = b.y + 5
+  })
 }
