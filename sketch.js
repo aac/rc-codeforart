b9ec93c47c2828dfa68365158d86ffd08cb796ee
Andrew Cove <andrew.cove@gmail.com>
2017-06-06 14:50:59 -0400

    Drawing random circles, sampling colors from the camera video image.

--- sketch.js
+++ sketch.js
@@ -13,10 +13,36 @@ function setup() {
   // to hide the capture outside of the canvas
   capture.hide()
 
+  frameRate(24)
+
   clear()
   background(51)
 }
 
 function draw() {
+  ellipseMode(RADIUS)
+  noStroke()
+  //clear()
+
+  capture.loadPixels()
+
+  var numCircles = 500
+  for (var i = 0; i < numCircles; i++){
+    var randX = random()
+    var randY = random()
+    var radius = random(20, 30)
+    //var radius = 30
+
+    var pixX = floor(randX * capture.width)
+    var pixY = floor(randY * capture.height)
+    var offset = (pixY * capture.width + pixX) * 4
+
+    fill(capture.pixels[offset], capture.pixels[offset+1],
+      capture.pixels[offset+2], 50)
+
+    ellipse(randX * width, randY*height, radius, radius)
+  }
+
   image(capture,0,0,320,240)
+
 }
