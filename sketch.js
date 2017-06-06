bd624305eba206d6f834259dbb1c3eb0e08186b0
Andrew Cove <andrew.cove@gmail.com>
2017-06-06 14:50:02 -0400

    Adding a video capture object and drawing the video inside the canvas.

--- sketch.js
+++ sketch.js
@@ -1,16 +1,22 @@
 /*
 
 */
+var capture
 
 function setup() {
   createCanvas(1280, 720)
+
+  capture = createCapture(VIDEO)
+  capture.size(320, 240)
+  // by default the capture object is displayed
+  // below the canvas, use the following command
+  // to hide the capture outside of the canvas
+  capture.hide()
+
   clear()
   background(51)
 }
 
 function draw() {
-  //clear()
-  //background(51)
-  //noStroke()
-  //ellipseMode(RADIUS)
+  image(capture,0,0,320,240)
 }
